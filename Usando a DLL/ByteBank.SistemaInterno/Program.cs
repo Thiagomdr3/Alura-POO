d45fadda77fab803cd76c8eba9e4b7ab0e73fe94
Thiago <thiago.cordeiro@localize.com.br>
2022-03-13 21:46:19 -0300

    first commit

---
+++ Usando a DLL/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,19 @@
+﻿using Excecoes;
+using System;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(554,321);
+            conta.Depositar(50);
+            Console.WriteLine(conta.Saldo);
+            DateTime data = new DateTime();  
+
+           
+            
+        }
+    }
+}
